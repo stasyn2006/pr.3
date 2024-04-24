6775a27e5ffbff6320b41e7b226531f13f2928ef
stasyn2006 <stanislav1544j@gmail.com>
2024-04-24 20:38:33 +0300

    Add first task

---
+++ Second.java
@@ -0,0 +1,12 @@
+public class Second {
+    public static void main (String[] args) {
+        int a,b;
+            a=1;
+            b=10;
+            for(int i=1;i<=10;i++){
+                a=a*i;
+            }
+            System.out.println(b+"!="+a);
+        }
+    
+    }
---
+++ third.java
@@ -0,0 +1,8 @@
+public class third {
+    public static void main(String[] args){
+        int i=500;
+        for(;i<=650;i=i+10){
+            System.out.println(i);
+        }
+    }
+}
